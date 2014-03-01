30bd62e364c0e71b7c32f1e5bfd079576cc05490
ThibaudD <thibaud.de@gmail.com>
2014-03-01 14:16:51 +0100

    Size option instead of classNameSize

--- bootbox.js
+++ bootbox.js
@@ -574,7 +574,7 @@
     options = sanitize(options);
 
     var dialog = $(templates.dialog);
-    var dialogSize = dialog.find(".modal-dialog");
+    var innerDialog = dialog.find(".modal-dialog");
     var body = dialog.find(".modal-body");
     var buttons = options.buttons;
     var buttonStr = "";
@@ -601,8 +601,12 @@
       dialog.addClass(options.className);
     }
 
-    if (options.classNameSize) {
-      dialogSize.addClass(options.classNameSize);
+    if (options.size === "large") {
+      innerDialog.addClass("modal-lg");
+    }
+
+    if (options.size === "small") {
+      innerDialog.addClass("modal-sm");
     }
 
     if (options.title) {
